41dd0f682dac3988249f3d8966207c2fb8448eff
pablorotten <pablorotten@gmail.com>
2017-11-06 07:15:17 +0100

    Countdown Function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 jQuery(document).ready(function ($) {
-  console.log("testing");
+    function countdown() {
+        var timeleft = 61;
+        var downloadTimer = setInterval(function(){
+          $("#countdown").text(--timeleft);
+          if(timeleft <= 0){
+            clearInterval(downloadTimer);
+            $('#btnTrigger').click();
+          }
+        },1000);
+    }
+
+    countdown();
 });
